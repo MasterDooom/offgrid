eb6eb1076b7e24a590f291ef9ce8dec15583cf13
MasterDooom <95602174+MasterDooom@users.noreply.github.com>
2026-09-14 11:01:18 +0530

    fix: use Kotlin-compatible Nearby version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
-    implementation("com.google.android.gms:play-services-nearby:19.5.0")
+    implementation("com.google.android.gms:play-services-nearby:19.4.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
